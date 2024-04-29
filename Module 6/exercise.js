eaf73dfb5e161ebbca3cfe5529a619a4a275d5ef
LangeM36 <156849283+LangeM36@users.noreply.github.com>
2024-04-29 14:08:59 -0500

    Add files via upload

---
+++ Module 6/exercise.js
@@ -0,0 +1,28 @@
+function result() {
+  var score = 0;
+  if (document.getElementById("correct1"));
+  {
+    score++;
+  }
+
+  if (document.getElementById("correct2"));
+  {
+    score++;
+  }
+
+  if (document.getElementById("correct3"));
+  {
+    score++;
+  }
+
+  if (document.getElementById("correct4"));
+  {
+    score++;
+  }
+
+  if (document.getElementById("correct5"));
+  {
+    score++;
+  }
+  document.write("You got " + score + " questions right");
+}
